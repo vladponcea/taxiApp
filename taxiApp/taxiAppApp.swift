641c82cee4171f578d7517a5213cefa0477bfaff
vladponcea <vladponcea@icloud.com>
2022-06-24 13:19:44 +0300

    first commit

---
+++ taxiApp/taxiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  taxiAppApp.swift
+//  taxiApp
+//
+//  Created by Vladut Mihai Poncea on 24.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct taxiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
